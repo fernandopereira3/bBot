b7b904240de5d164f5f0c957bc90febed2281b73
Fernando Pereira <39241138+fernandopereira3@users.noreply.github.com>
2022-11-18 08:07:42 -0300

    Delete log.py

--- log.py
+++
@@ -1,8 +0,0 @@
-from distutils.debug import DEBUG
-import logging
-
-class Log:
-    def __init__(self):
-        self.padrao = logging.basicConfig(filename='/scripts/backup.log', format='%(message)s %(asctime)s',filemode='a',encoding='utf-8',level=DEBUG)
-
-nota = Log()
