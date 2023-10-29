086b06bf818d59c0a0cb1a66f21650c2b1b27328
Fernando <fernandopereira-3@hotmail.com>
2023-10-29 14:40:44 -0300

    Atualizacao

--- Backup.py
+++ Backup.py
@@ -8,14 +8,18 @@ log = logging.getLogger('rich')
 
 class Bkp():
         def backupSMB():
-                mount = os.system("mount -t nfs 10.14.180.4:/home/smb /home/source")
-                log.warning('Backup SMB inicializado!')
-                srcDir = ['/home/source/administrativo','/home/source/cimic','/home/source/comunicados','/home/source/cpd','/home/source/eat','/home/source/notes','/home/source/producao','/home/source/reintegracao','/home/source/seguranca']
+                # mount = os.system("mount -t nfs 10.14.180.4:/home/smb /home/source")
+                log.warning('Backup inicializado!')
+                with open('diretorios.txt', 'r') as srDir:
+                        srDir.readline()
+                # srcDir = ['/home/source/administrativo','/home/source/cimic','/home/source/comunicados','/home/source/cpd','/home/source/eat','/home/source/notes','/home/source/producao','/home/source/reintegracao','/home/source/seguranca']
                 bkpDir = '/home/bkp/smb/'
                 i = 0
-                for i in range(0,len(srcDir)):
-                        bkp_rotina = ['rsync','-hadzPu', srcDir[i], bkpDir] 
-                        processo = subprocess.run(bkp_rotina)   
+                for i in range(0,len(srDir)):
+                        bkp_rotina = ['rsync','-hadzPu', srDir[i], bkpDir] 
+                        processo = subprocess.run(bkp_rotina) 
+                srDir.close()  
+                
                 os.system('chmod -R 777 /home/bkp/')
                 umount = os.system('umount -l 10.14.180.4:/home/smb')
                 log.warning('Backup SMB Completo')
@@ -24,31 +28,16 @@ class Bkp():
                 mount = os.system("mount -t nfs 10.14.180.4:/home/bibliotecas /home/source")
                 log.warning('Backup BIBLIOTECAS inicializado!')
                 #/adm /aevp /chefia /cimic /cpd /cras /financas /inclusao /infra /judiciaria /peculio /pessoal /portaria /producao /rol /saude /seguranca /supervisao 
-                srcDir = ['/home/source/adm',
-                          '/home/source/aevp',
-                          '/home/source/chefia',
-                          '/home/source/cimic',
-                          '/home/source/cpd',
-                          '/home/source/cras',
-                          '/home/source/financas',
-                          '/home/source/inclusao',
-                          '/home/source/infra',
-                          '/home/source/judiciaria',
-                          '/home/source/peculio',
-                          '/home/source/pessoal',
-                          '/home/source/portaria',
-                          '/home/source/producao',
-                          '/home/source/rol',
-                          '/home/source/saude',
-                          '/home/source/seguranca',
-                          '/home/source/supervisao']
+                with open('diretorios.txt', 'r') as srDir:
+                        srDir.readline()
 
                 bkpDir = '/home/bkp/bibliotecas/'
                 i = 0
-                for i in range(0,len(srcDir)):
-                        bkp_rotina = ['rsync','-hadzPu', srcDir[i], bkpDir] 
+                for i in range(0,len(srDir)):
+                        bkp_rotina = ['rsync','-hadzPu', srDir[i], bkpDir] 
                         processo = subprocess.run(bkp_rotina) 
 
+                srDir.close()
                 os.system('chmod -R 777 /home/bkp/bibliotecas')
                 umount = os.system('umount -l 10.14.180.4:/home/bibliotecas')
                 log.warning('Backup BIBLIOTECAS Completo!')
