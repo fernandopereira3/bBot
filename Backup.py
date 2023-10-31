fc4a0525f8fea7e3ff61ac02911c123da270f8e8
Fernando <fernandopereira-3@hotmail.com>
2023-10-31 15:50:50 -0300

    Path walk com problemas

--- Backup.py
+++ Backup.py
@@ -6,13 +6,13 @@ import rich
 
 # FORMAT = '%(asctime)s %(message)s'
 # logging.basicConfig(level='NOTSET', format=FORMAT, datefmt='[%d/%b - %H:%M]', filename='/scripts/backup.log',filemode='a', encoding='utf-8')
-log = logging.getLogger('rich')
+# log = logging.getLogger('rich')
 
 
 def bucket():
     client = boto3.client(
         service_name='s3',
-        
+
         region_name='us-west-2'
     )
 
@@ -20,13 +20,13 @@ def bucket():
 class Bkp:
 
     def backupSMB():
-        # mount = os.system("mount -t nfs 10.14.180.4:/home/smb /home/source")
-        log.warning('Backup inicializado!')
-        # with open('diretorios.txt', 'r') as source:
-        #     source.readline()
+        # source = os.system("mount -t nfs -o resvport,rw 10.0.0.140:/home/arquivos ~/mount/arquivos ")
+        # log.warning('Backup inicializado!')
+        with open('diretorios.txt', 'r') as source:
+            source.readline()
         # srcDir = ['/home/source/administrativo','/home/source/cimic','/home/source/comunicados','/home/source/cpd','/home/source/eat','/home/source/notes','/home/source/producao','/home/source/reintegracao','/home/source/seguranca']
         # bkpDir = '/home/bkp/smb/'
-        source = "/Users/fernando/Documents"
+        # source = "/Users/fernando/Documents"
         for (dirpath, dirnames, filenames) in os.walk(source):
             for f in filenames:
                 f_path = os.path.join(dirpath, f)
@@ -36,6 +36,10 @@ class Bkp:
         if f_size_kb >= 2.000:
             print(f_path)
 
+        
+        # umount = os.system("umount 10.0.0.140:/home/arquivos")
+
+        
         # i = 0
         # for i in range(0, len(source)):
         #     bkp_rotina = ['rsync', '-hadzPu',b    
