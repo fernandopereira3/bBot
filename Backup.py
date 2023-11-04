7fee84f272cc7623111d3456b78827b1882be702
Fernando <fernandopereira-3@hotmail.com>
2023-11-03 23:15:21 -0300

    Renovado enviando os arquivos ao S3

--- Backup.py
+++ Backup.py
@@ -1,51 +1,28 @@
-import subprocess
 import os
-import logging
 import boto3
-import rich
-
-# FORMAT = '%(asctime)s %(message)s'
-# logging.basicConfig(level='NOTSET', format=FORMAT, datefmt='[%d/%b - %H:%M]', filename='/scripts/backup.log',filemode='a', encoding='utf-8')
-# log = logging.getLogger('rich')
-
-
-def bucket():
-    client = boto3.client(
-        ##########
-        service_name='s3',
-       
-        region_name='us-west-2'
-    )
-
 
 class Bkp:
 
-    def backupSMB():
-        mount = os.system("mount -t nfs -o resvport,rw 10.0.0.140:/home/arquivos ~/mount/arquivos")
-        # log.warning('Backup inicializado!')
-        source = open('diretorios.txt', 'r')
-        leitor = source.readline()
-        # srcDir = ['/home/source/administrativo','/home/source/cimic','/home/source/comunicados','/home/source/cpd','/home/source/eat','/home/source/notes','/home/source/producao','/home/source/reintegracao','/home/source/seguranca']
-        # bkpDir = '/home/bkp/smb/'
-        # source = "/Users/fernando/Documents"
-        for (dirpath, dirnames, filenames) in os.walk(str(leitor)):
-            for f in filenames:
-                f_path = os.path.join(dirpath, f)
-                f_size = os.path.getsize(f_path)
-                f_size_kb = f_size / 1024
-                # return print("PATH: " + f_path + "TAMANHO: " + f_size_kb)
-                
-                # return print(f_size_kb)
+    def backupSMB(self):
+         with open('diretorios.txt', 'r') as source:
+            for leitor in source.readlines():
+                directory_path = leitor.strip()
+
+                for (dirpath, dirnames, filenames) in os.walk(directory_path):
+                    for filename in filenames:
+                        file_path = os.path.join(dirpath, filename)
+                        file_size = os.path.getsize(file_path) / (1024 * 1024)  # Tamanho em MB
+                        if file_size <= 1  and (filename.endswith('.pdf') or filename.endswith('.txt')):
+                            s3_client.upload_file(file_path, 'bbot-s3', filename)
+                            print(f'Arquivo {filename} ({file_size:.2f} MB) enviado para o Amazon S3.')
+
+
+if __name__ == "__main__":
+    s3_client = boto3.client(
+        's3',
         
-        source.close()
-        umount = os.system("umount 10.0.0.140:/home/arquivos")
-        
+        region_name='us-west-2'
+    )
 
-
-
-        # if f_size_kb >= 2.000:
-        #     print(f_path)
-     
-        # i = 0
-        # for i in range(0, len(source)):
-        #     bkp_rotina = ['rsync', '-hadzPu',b    
+    backup = Bkp()
+    backup.backupSMB() 
